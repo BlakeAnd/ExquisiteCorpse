e606a35ee018c9c9fbfd6e106b6a9ad61654f073
Blake Mitchell <mblakeandrew@gmail.com>
2020-03-28 16:54:24 -0700

    moved starting variable to draw file

--- startremote.js
+++ startremote.js
@@ -1,22 +1,22 @@
-let canvas_selection = ".";
+// let canvas_selection = ".";
 
-function selectedRandom() {
-  let rand = Math.random();
-  if (rand < 0.5){
-    selectedTop();
-  } 
-  else {
-    selectedBottom();
-  }
-}
+// function selectedRandom() {
+//   let rand = Math.random();
+//   if (rand < 0.5){
+//     selectedTop();
+//   } 
+//   else {
+//     selectedBottom();
+//   }
+// }
 
-function selectedTop() {
-  canvas_selection = "top";
-  window.location.assign(`https://drawexquisitecorpse.netlify.com/remotedraw`);
-}
+// function selectedTop() {
+//   canvas_selection = "top";
+//   window.location.assign(`https://drawexquisitecorpse.netlify.com/remotedraw`);
+// }
 
-function selectedBottom() {
-  canvas_selection = "bottom";
-  window.location.assign(`https://drawexquisitecorpse.netlify.com/remotedraw`);
-}
+// function selectedBottom() {
+//   canvas_selection = "bottom";
+//   window.location.assign(`https://drawexquisitecorpse.netlify.com/remotedraw`);
+// }
 
