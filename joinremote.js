1bb21e6d8306a9a4bf331b5782891b912c045990
Blake Mitchell <mblakeandrew@gmail.com>
2020-03-31 16:34:30 -0700

    logging substrings from query string

--- joinremote.js
+++ joinremote.js
@@ -2,3 +2,7 @@ const queryString = window.location.search;
 
 console.log("joining", typeof(queryString), queryString);
 
+let pair_id = queryString.substring(1, 7)
+let canvas_selection = queryString.substring(7, 12)
+console.log("id", pair_id, "canv", canvas_selection);
+localStorage.setItem();
