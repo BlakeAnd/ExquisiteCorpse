0e0698ed51faabb2a6f7591a617a0d4330325403
Blake Mitchell <mblakeandrew@gmail.com>
2020-03-31 16:36:33 -0700

    changed index on substring

--- joinremote.js
+++ joinremote.js
@@ -3,6 +3,6 @@ const queryString = window.location.search;
 console.log("joining", typeof(queryString), queryString);
 
 let pair_id = queryString.substring(1, 7)
-let canvas_selection = queryString.substring(7, 12)
+let canvas_selection = queryString.substring(7, 14)
 console.log("id", pair_id, "canv", canvas_selection);
 localStorage.setItem();
