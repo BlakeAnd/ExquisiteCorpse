1f4785e3910d4e0990bfd5e91d00ffc05931703e
Blake Mitchell <mblakeandrew@gmail.com>
2020-03-31 16:39:10 -0700

    adjusted substring, set values to local storage

--- joinremote.js
+++ joinremote.js
@@ -3,6 +3,8 @@ const queryString = window.location.search;
 console.log("joining", typeof(queryString), queryString);
 
 let pair_id = queryString.substring(1, 7)
-let canvas_selection = queryString.substring(7, 14)
+let canvas_selection = queryString.substring(8, 14)
 console.log("id", pair_id, "canv", canvas_selection);
-localStorage.setItem();
+
+localStorage.setItem("pair_id", pair_id);
+localStorage.setItem("canvas_selection", canvas_selection);
