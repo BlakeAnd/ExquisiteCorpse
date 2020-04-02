fbea54a784d0d532a37972b5c79bffab6b4adabb
Blake Mitchell <mblakeandrew@gmail.com>
2020-04-02 14:07:34 -0700

    changed joining game file to reverse the canvas selection

--- joinremote.js
+++ joinremote.js
@@ -6,7 +6,12 @@ let pair_id = queryString.substring(1, 7)
 let canvas_selection = queryString.substring(8, 14)
 console.log("id", pair_id, "canv", canvas_selection);
 
+if(canvas_selection = "top"){
+  localStorage.setItem("canvas_selection", "bottom");
+} else{
+  localStorage.setItem("canvas_selection", "top");
+}
+
 localStorage.setItem("pair_id", pair_id);
-localStorage.setItem("canvas_selection", canvas_selection);
 localStorage.setItem("player_status", "joining");
 window.location.assign(`https://drawexquisitecorpse.netlify.com/remotedraw`);
