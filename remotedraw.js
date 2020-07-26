26003a1e35e9c63328830a2edf8332e9e315ac03
Blake Mitchell <mblakeandrew@gmail.com>
2020-07-25 21:33:55 -0700

    changed bound of get image function

--- remotedraw.js
+++ remotedraw.js
@@ -51,7 +51,7 @@ function combine_canvases () {
 
   //   // var ctx3 = can3.getContext('2d'); 
 
-  let combinedImageData = combined_context.getImageData(0,0, 20, 40);
+  let combinedImageData = combined_context.getImageData(0,0, 200, 400);
 
 
   let pair_id = localStorage.getItem("pair_id");
