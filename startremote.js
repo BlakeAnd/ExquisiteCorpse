7b564f8b1ab91b8b0135bd80c546bfdec7e4dbcc
Blake Mitchell <mblakeandrew@gmail.com>
2020-08-17 18:37:05 -0700

    added logic for checking if url is unique and regenerating if it is not

--- startremote.js
+++ startremote.js
@@ -1,5 +1,5 @@
 // const { default: Axios } = require("axios");
-let id_array = null;
+let id_obj = {};
 let deployed = "https://drawexquisitecorpse.herokuapp.com";
 let local = "http://localhost:5000";
 let url = local;
@@ -8,14 +8,20 @@ axios({
   url: `${url}/every_canvas_id`
 })
 .then( res => {
-  console.log(res)
+  console.log(res) 
+  let id_arr = res.data;
+  for(let i = 0; i < id_arr.length; i++){
+    let current_id = id_arr[i].drawing_canvas;
+    id_obj[current_id] =  true;
+  }
+  make_id();
 })
 .catch( err => {
   console.log("pinged err", err);
 })
 
 localStorage.setItem("player_status", "starting")
-makeid();
+
 
 
 
@@ -41,12 +47,17 @@ function selectedBottom() {
   window.location.assign(`https://drawexquisitecorpse.netlify.com/remotedraw`);
 }
 
-function makeid() {
+let safety_counter = 0;
+function make_id() {
+  safety_counter ++;
   let len = 6;
   let text = "";
   let char_list = "abcdefghijklmnopqrstuvwxyz";
   for(var i=0; i < len; i++ ){  
     text += char_list.charAt(Math.floor(Math.random() * char_list.length));
+  }
+  if(id_obj[text] === true && safety_counter < 1000){
+    make_id();
   }
   console.log(text);
   localStorage.setItem("pair_id", text);
