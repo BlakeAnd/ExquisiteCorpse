f7dc6ae73c8106ab693507dfbe9f3a00c413a759
BlakeAnd <mblakeandrew@gmail.com>
2023-06-26 18:08:26 -0700

    removed logic attempting to check previous url for different redirects in join file

--- joinremote.js
+++ joinremote.js
@@ -10,9 +10,9 @@ localStorage.setItem("pair_id", pair_id);
 localStorage.setItem("canvas_selection", canvas_selection);
 localStorage.setItem("player_status", "joining"); 
 
-if(document.referrer.length > 0){
+// if(document.referrer.length > 0){
   window.location.assign(`https://drawexquisitecorpse.netlify.com/remotedraw`);
-}
-else{
-  window.location.assign(`file:///D:/Blake/Documents/Moved%20Docs/Git/ExquisiteCorpseProject/ExquisiteCorpse/remotedraw.html`);
-}
+// }
+// else{
+//   window.location.assign(`file:///D:/Blake/Documents/Moved%20Docs/Git/ExquisiteCorpseProject/ExquisiteCorpse/remotedraw.html`);
+// }
