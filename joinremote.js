5e547ba3688be3dd8f5104256a14e7375e1bce2c
BlakeAnd <mblakeandrew@gmail.com>
2023-06-27 09:11:26 -0700

    re-added redirect to join page

--- joinremote.js
+++ joinremote.js
@@ -11,7 +11,7 @@ localStorage.setItem("canvas_selection", canvas_selection);
 localStorage.setItem("player_status", "joining"); 
 
 // if(document.referrer.length > 0){
-  // window.location.assign(`https://drawexquisitecorpse.netlify.com/remotedraw`);
+window.location.assign(`https://drawexquisitecorpse.netlify.com/remotedraw`);
 // }
 // else{
 //   window.location.assign(`file:///D:/Blake/Documents/Moved%20Docs/Git/ExquisiteCorpseProject/ExquisiteCorpse/remotedraw.html`);
